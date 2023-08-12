2c880197eba59f5a726ce30ce9a3c45688f95d11
SupriyaVaniM <supriyavanim@gmail.com>
2023-08-12 09:19:47 +0530

    udated expanded view UI

--- lib/views/expanded_news_page.dart
+++ lib/views/expanded_news_page.dart
@@ -8,7 +8,8 @@ class HomePage extends StatefulWidget {
 
   final String imageUrl;
 
-  const HomePage({super.key, 
+  const HomePage({
+    super.key,
     required this.title,
     required this.description,
     required this.publishedAt,
@@ -22,142 +23,66 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      body: CustomScrollView(
-        slivers: [
-          // Sliver app Bar
-          SliverAppBar(
-            title: Text(
-              widget.title,
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),
-            leading: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: InkWell(
-                onTap: () {
-                  Navigator.pop(context);
-                },
-                child: CircleAvatar(
-                  backgroundColor: Colors.black.withOpacity(0.4),
-                  child: const Icon(Icons.chevron_left, color: Colors.white),
-                ),
-              ),
-            ),
-            centerTitle: true,
-            expandedHeight: 300,
-            flexibleSpace: Stack(children: [
-              FlexibleSpaceBar(
-                background: ShaderMask(
-                  shaderCallback: (bounds) => const LinearGradient(
-                          colors: [Colors.black38, Colors.black],
-                          begin: Alignment.center,
-                          end: Alignment.bottomCenter)
-                      .createShader(bounds),
-                  blendMode: BlendMode.darken,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: CachedNetworkImageProvider(
-                              widget.imageUrl,
-                              errorListener: () {
-                                const Text(
-                                  'Loading',
-                                  style: TextStyle(
-                                    color: Colors.blue,
-                                    fontSize: 20,
-                                  ),
-                                );
-                              },
-                            ))),
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: CircleAvatar(
+                    backgroundColor: Colors.black.withOpacity(0.4),
+                    child: const Icon(Icons.chevron_left, color: Colors.white),
                   ),
                 ),
               ),
-              Container(
-                margin: const EdgeInsets.only(
-                  left: 20,
-                  top: 180,
-                  right: 40,
-                ),
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        widget.title,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 30,
-                            fontWeight: FontWeight.w600),
-                      ),
-                      Text(
-                        widget.description,
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.normal),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Row(children: [
-                        const Icon(
-                          Icons.newspaper_sharp,
-                          color: Colors.white,
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        const Spacer(),
-                        const Icon(
-                          Icons.calendar_month_outlined,
-                          color: Colors.white,
-                        ),
-                        Text(
-                          widget.publishedAt,
-                          style:
-                              const TextStyle(color: Colors.white, fontSize: 8),
-                        )
-                      ])
-                    ],
-                  ),
-                ),
-              )
-            ]),
-          ),
-          SliverToBoxAdapter(
-            child: Container(
-              margin: const EdgeInsets.only(left: 20, right: 20, top: 40),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "News Content",
-                    style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.grey[600]),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
+    
+        ),
+        backgroundColor: Colors.grey[200],
+        body: Stack(
+          children:[
+            Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: CachedNetworkImageProvider(
+                                  widget.imageUrl,
+                                  errorListener: () {
+                                    const Text(
+                                      'Loading',
+                                      style: TextStyle(
+                                        color: Colors.blue,
+                                        fontSize: 20,
+                                      ),
+                                    );
+                                  },
+                                ))),),
+                                 Padding(
+                                   padding: const EdgeInsets.only(top:12.0),
+                                   child: Align(
+                                     alignment: Alignment.topCenter,
+                                     child: Text(
+                                                           widget.title,
+                                                           maxLines: 1,
+                                                           overflow: TextOverflow.ellipsis,
+                                                           style: const TextStyle(
+                                                               color: Colors.white,
+                                                               fontSize: 20,
+                                                               fontWeight: FontWeight.w600),
+                                                         ),
+                                   ),
+                                 ),
+          ] 
+        )),
     );
+     
+          
+        
+      
+    
   }
 }
